d6cfc2bdf2b69b0fec7236ac15f87e358c4b6fd9
Gustavo Soares <gustavohsoaressilva@gmail.com>
2023-03-23 22:13:42 -0300

    Adicionado função de ler e escrever, introduzido json

--- files/main.dart
+++ files/main.dart
@@ -1,24 +1,35 @@
+import 'dart:convert';
 import 'dart:io';
 
 void main() {
   String nome = "Gustavo";
   List<String> nomes = [nome, "bia", "ulisses"];
-  File file = File("nomes.txt");
-  file.writeAsStringSync(nomes.join(', '));
 
-  String nomesTexto = file.readAsStringSync();
+  escreverArquivoTexto("nomes.txt", nomes.join(", "));
+
+  String nomesTexto = lerArquivoTexto("nomes.txt");
   List<String> nomesSalvos = nomesTexto.split(', ');
   print(nomesSalvos);
 
   // const string = 'Hello world !';
   // final splitted = string.split(' ');
   // print(splitted); // [Hello, world!];
-
   List<String> frutas = ['banana', 'maçã', 'pera', 'uva', 'limão'];
-  File fileFrutas = File('frutas.txt');
-  fileFrutas.writeAsStringSync(frutas.join('\n'));
+  String json = jsonEncode(frutas);
+  escreverArquivoTexto("frutas.json", json);
 
-  String frutasTexto = fileFrutas.readAsStringSync();
+  String frutasTexto = lerArquivoTexto("frutas.txt");
   List<String> frutasSalvas = frutasTexto.split('\n');
   print(frutasSalvas);
+}
+
+void escreverArquivoTexto(String arquivo, String informacoes) {
+  File file = File(arquivo);
+  file.writeAsStringSync(informacoes);
+}
+
+String lerArquivoTexto(String arquivo) {
+  File file = File(arquivo);
+  String informacoes = file.readAsStringSync();
+  return informacoes;
 }
