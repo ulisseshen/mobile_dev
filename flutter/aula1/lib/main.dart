587498b3327fc80cc2db0a004a82a5f366a5241d
NatanTG <natatidioligirotto@gmail.com>
2023-04-20 22:06:50 -0300

    criado widget complexo com container - aula5

--- flutter/aula1/lib/main.dart
+++ flutter/aula1/lib/main.dart
@@ -1,46 +1,83 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // runApp(Container(
-  //   color: Colors.blue,
-  //   child: Padding(
-  //     padding: const EdgeInsets.all(8.0),
-  //     child: Center(
-  //       child: Container(
-  //         color: Colors.red,
-  //         width: 300,
-  //         height: 300,
-  //         child: const Center(
-  //           child: Text(
-  //             "L",
-  //             style: TextStyle(color: Colors.white),
-  //             textDirection: TextDirection.ltr,
-  //           ),
-  //         ),
-  //       ),
-  //     ),
-  //   ),
-  // ));
-
-  runApp(
-    MaterialApp(
-      title: "app da aula1",
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.account_circle),
-          title: Text("aula 1"),
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(12.0),
-          child: Column(
-            children: [
-              Text("Aula 1 2 3"),
-              Icon(Icons.account_circle),
-              Text("aula 4 5 6"),
-            ],
+  runApp(MaterialApp(
+    title: "Aula 4",
+    home: Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: 30,
           ),
-        ),
+          Container(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: 10,
+                          height: 10,
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.all(Radius.circular(50)),
+                          ),
+                        ),
+                      ),
+                      width: 50,
+                      height: 50,
+                      margin: EdgeInsets.only(left: 10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15.0, bottom: 10.0),
+                      child: Text(
+                        "Útilma mensagem",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "dilma",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            width: 300,
+            height: 100,
+            decoration: BoxDecoration(
+              color: Colors.blueAccent,
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+              border: Border.all(width: 5, color: Colors.red),
+            ),
+          ),
+        ],
       ),
     ),
-  );
+  ));
 }
