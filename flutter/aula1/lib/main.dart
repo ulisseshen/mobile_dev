5e7eeb6957f8e8040ad9e6e1a58351ee490979cb
NatanTG <natatidioligirotto@gmail.com>
2023-05-08 17:18:31 -0300

    modificação nenhuma

--- flutter/aula1/lib/main.dart
+++ flutter/aula1/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   final email = 'email5454@essssol.com';
+  final senha = '44455';
 
   runApp(MaterialApp(
     title: 'Aula 4',
@@ -104,6 +105,7 @@ void main() {
           SizedBox(height: 20),
           MensagemBolsonaro(),
           SizedBox(height: 20),
+          Senha()
         ],
       ),
     ),
@@ -243,6 +245,120 @@ class MensagemDilma extends StatelessWidget {
                   color: Colors.white,
                   fontSize: 15,
                 ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//criar widget de senha statefull com manipulação de estado do obscureText e validção de senha com 6 caracteres
+class Senha extends StatefulWidget {
+  const Senha({
+    super.key,
+  });
+
+  @override
+  _SenhaState createState() => _SenhaState();
+}
+
+class _SenhaState extends State<Senha> {
+  bool obscureText = true;
+  String senha = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 100,
+      margin: EdgeInsets.symmetric(horizontal: 10.0).copyWith(top: 10.0),
+      decoration: BoxDecoration(
+        color: Colors.blueAccent,
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+        border: Border.all(width: 5, color: Colors.red),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 50,
+                height: 50,
+                margin: EdgeInsets.only(left: 10.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.person_2_sharp,
+                    size: 30,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Senha',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                senha,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(
+                icon: Icon(Icons.remove_red_eye),
+                onPressed: () {
+                  setState(() {
+                    obscureText = !obscureText;
+                  });
+                },
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  setState(() {
+                    senha += '*';
+                  });
+                },
               ),
             ],
           ),
