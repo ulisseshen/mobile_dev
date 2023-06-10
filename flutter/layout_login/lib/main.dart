0aaee61a26ded07ceea006401db24ebffe3bc977
Gustavo Soares <gustavohsoaressilva@hens.com.br>
2023-06-09 21:35:24 -0300

    Arquitetura inicial das páginas do projeto

--- flutter/layout_login/lib/main.dart
+++ flutter/layout_login/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'src/pages/login_screen_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,139 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 30),
-          child: Column(children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Skip',
-                    style: TextStyle(
-                      color: Color(0xff0088CC),
-                      fontWeight: FontWeight.w500,
-                      fontSize: 20,
-                      fontFamily: 'Inter',
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 100,
-            ),
-            const Text(
-              'MobileDev',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 36,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              'Aprenda Rápido, Evolua rápido',
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 18,
-              ),
-            ),
-            const SizedBox(
-              height: 100,
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 32,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                onPressed: () {},
-                child: const Text('Login'),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 32,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                onPressed: () {},
-                child: const Text('Registrar'),
-              ),
-            ),
-            const SizedBox(height: 30),
-            Row(
-              children: const [
-                Expanded(
-                  child: Divider(
-                    color: Color(0xff0088CC),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15),
-                  child: Text(
-                    'Logar com',
-                    style: TextStyle(fontWeight: FontWeight.w500),
-                  ),
-                ),
-                Expanded(
-                  child: Divider(
-                    color: Color(0xff0088CC),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 22,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.facebook,
-                    color: Color(0xff0088CC),
-                    size: 32,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 33),
-                  child: Container(
-                    color: Colors.black,
-                    height: 32,
-                    width: 1,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.apple,
-                    color: Color(0xff0088CC),
-                    size: 32,
-                  ),
-                ),
-              ],
-            ),
-          ]),
-        ),
-      ),
+      home: LoginScreenPage(),
     );
   }
 }
