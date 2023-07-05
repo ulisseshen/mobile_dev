ff9dd8167a78d3303d006320d115341c120dec51
Gustavo Soares <gustavohsoaressilva@hens.com.br>
2023-07-05 20:19:48 -0300

    Adicionado tamanho proporcional no middle; Iniciado widget bottom

--- flutter/ecommerce_app/lib/main.dart
+++ flutter/ecommerce_app/lib/main.dart
@@ -33,78 +33,114 @@ class Onboarding extends StatelessWidget {
             colors: [Color(0xFFCE048C), Color(0xFF4D0A8E)],
           ),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _Header(),
-            SizedBox(
-              height: 130 - 22,
-            ),
-            _Middle(),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30).copyWith(top: 80),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _Header(),
+              SizedBox(
+                height: 130 - 22,
+              ),
+              _Middle(),
+              _Bottom()
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class _Middle extends StatelessWidget {
-  const _Middle({
-    super.key,
-  });
+class _Bottom extends StatelessWidget {
+  const _Bottom({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      clipBehavior: Clip.none,
+    return Column(
       children: [
-        Container(
-          height: 142,
+        Text(
+          "*Valid from 27/03 to 01/04 2022. Min stock: 1 unit",
+          style: TextStyle(color: Colors.white, fontSize: 8),
         ),
-        Positioned(
-          top: 20,
-          child: Image.asset(
-            "imagens/mac.png",
-            height: 142,
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              minimumSize: Size.fromHeight(48),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(65))),
+          onPressed: () {},
+          child: Text(
+            "LOG IN",
+            style: TextStyle(color: Color(0xFFFF5500)),
           ),
-        ),
-        Positioned(
-            top: 220,
-            left: 170,
-            child: Image.asset(
-              "imagens/controlezin.png",
-              height: 80,
-            )),
-        Positioned(
-          top: 80,
-          left: 110,
-          child: Image.asset(
-            "imagens/nintendo.png",
-            height: 142,
-          ),
-        ),
-        Positioned(
-          top: -25,
-          left: 160,
-          child: Image.asset(
-            "imagens/headphone.png",
-            height: 148,
-          ),
-        ),
-        Positioned(
-            top: 190,
-            child: Image.asset(
-              "imagens/alexa.png",
-              height: 65,
-            )),
-        Positioned(
-            top: 220,
-            left: 80,
-            child: Image.asset(
-              "imagens/foneblutu.png",
-              height: 90,
-            )),
+        )
       ],
+    );
+  }
+}
+
+class _Middle extends StatelessWidget {
+  const _Middle();
+
+  @override
+  Widget build(BuildContext context) {
+    final widthFactor = MediaQuery.of(context).size.width * 0.8;
+    final heightFactor = MediaQuery.of(context).size.height * 0.45;
+    return Align(
+      alignment: Alignment.center,
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          SizedBox(
+            width: widthFactor,
+            height: heightFactor,
+          ),
+          Positioned(
+            top: 20,
+            child: Image.asset(
+              "imagens/mac.png",
+              height: 142,
+            ),
+          ),
+          Positioned(
+              top: 220,
+              left: 170,
+              child: Image.asset(
+                "imagens/controlezin.png",
+                height: 80,
+              )),
+          Positioned(
+            top: 80,
+            left: 110,
+            child: Image.asset(
+              "imagens/nintendo.png",
+              height: 142,
+            ),
+          ),
+          Positioned(
+            top: -25,
+            left: 160,
+            child: Image.asset(
+              "imagens/headphone.png",
+              height: 148,
+            ),
+          ),
+          Positioned(
+              top: 190,
+              child: Image.asset(
+                "imagens/alexa.png",
+                height: 65,
+              )),
+          Positioned(
+              top: 220,
+              left: 80,
+              child: Image.asset(
+                "imagens/foneblutu.png",
+                height: 90,
+              )),
+        ],
+      ),
     );
   }
 }
