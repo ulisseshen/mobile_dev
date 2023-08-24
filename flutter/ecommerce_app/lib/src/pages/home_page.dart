628cf88ed673dc71d62d23a047767343e64383a5
Natã Girotto <nata.girotto@hens.com>
2023-08-24 20:56:02 -0300

    feat: implementado bottomNavigation nativo e customizado

--- flutter/ecommerce_app/lib/src/pages/home_page.dart
+++ flutter/ecommerce_app/lib/src/pages/home_page.dart
@@ -3,10 +3,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final Color grayColor = const Color(0xFFCFCFCF);
+
   final Color orangeColor = const Color(0xFFFF5500);
+
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -70,133 +79,232 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      bottomNavigationBar: SizedBox(
+
+      bottomNavigationBar: Container(
         height: 110,
-        child: ClipRRect(
+        decoration: BoxDecoration(
+          color: Colors.white,
           borderRadius: BorderRadius.only(
               topLeft: Radius.circular(40), topRight: Radius.circular(40)),
-          child: BottomNavigationBar(
-              currentIndex: 0,
-              showSelectedLabels: false,
-              showUnselectedLabels: false,
-              unselectedIconTheme: IconThemeData(color: grayColor),
-              items: [
-                BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.home,
-                    ),
-                    activeIcon: Container(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                      decoration: BoxDecoration(
-                          color: orangeColor.withOpacity(0.1),
-                          borderRadius: BorderRadius.all(Radius.circular(50))),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(
-                            Icons.home,
-                            color: orangeColor,
-                          ),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Text("Home",
-                              style: TextStyle(
-                                color: orangeColor,
-                              ))
-                        ],
+        ),
+        child: Material(
+          color: Colors.transparent,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              //index 0
+              _currentIndex == 0
+                  ? ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.home,
+                        color: orangeColor,
                       ),
-                    ),
-                    label: ""),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.shopping_bag),
-                    activeIcon: Container(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                      decoration: BoxDecoration(
-                          color: orangeColor.withOpacity(0.1),
-                          borderRadius: BorderRadius.all(Radius.circular(50))),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(
-                            Icons.shopping_bag,
-                            color: orangeColor,
-                          ),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Text("Bag",
-                              style: TextStyle(
-                                color: orangeColor,
-                              ))
-                        ],
+                      label: Text(
+                        "Home",
+                        style: TextStyle(
+                          color: orangeColor,
+                        ),
                       ),
-                    ),
-                    label: ""),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.favorite),
-                    activeIcon: Container(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                      decoration: BoxDecoration(
-                          color: orangeColor.withOpacity(0.1),
-                          borderRadius: BorderRadius.all(Radius.circular(50))),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(
-                            Icons.favorite,
-                            color: orangeColor,
-                          ),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Text(
-                            "Favorite",
-                            style: TextStyle(
-                              color: orangeColor,
-                            ),
-                          )
-                        ],
+                      style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        backgroundColor: orangeColor.withOpacity(0.1),
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50))),
                       ),
-                    ),
-                    label: ""),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.person),
-                    activeIcon: Container(
-                      margin: EdgeInsets.only(right: 8),
-                      padding:
-                          EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                      decoration: BoxDecoration(
-                          color: orangeColor.withOpacity(0.1),
-                          borderRadius: BorderRadius.all(Radius.circular(50))),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Icon(
-                            Icons.person,
-                            color: orangeColor,
-                          ),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Text("User",
-                              style: TextStyle(
-                                color: orangeColor,
-                              ))
-                        ],
+                    )
+                  : IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _currentIndex = 0;
+                        });
+                      },
+                      icon: Icon(
+                        Icons.home,
+                        color: grayColor,
+                      )),
+              //index 1
+              _currentIndex == 1
+                  ? ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.shopping_bag,
+                        color: orangeColor,
                       ),
-                    ),
-                    label: "")
-              ]),
+                      label: Text(
+                        "Bag",
+                        style: TextStyle(
+                          color: orangeColor,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        backgroundColor: orangeColor.withOpacity(0.1),
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50))),
+                      ),
+                    )
+                  : IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _currentIndex = 1;
+                        });
+                      },
+                      icon: Icon(
+                        Icons.shopping_bag,
+                        color: grayColor,
+                      )),
+              //index 2
+              _currentIndex == 2
+                  ? ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.favorite,
+                        color: orangeColor,
+                      ),
+                      label: Text(
+                        "Favorite",
+                        style: TextStyle(
+                          color: orangeColor,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        backgroundColor: orangeColor.withOpacity(0.1),
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50))),
+                      ),
+                    )
+                  : IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _currentIndex = 2;
+                        });
+                      },
+                      icon: Icon(
+                        Icons.favorite,
+                        color: grayColor,
+                      )),
+              //index 3
+              _currentIndex == 3
+                  ? ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.person,
+                        color: orangeColor,
+                      ),
+                      label: Text(
+                        "User",
+                        style: TextStyle(
+                          color: orangeColor,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        backgroundColor: orangeColor.withOpacity(0.1),
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50))),
+                      ),
+                    )
+                  : IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _currentIndex = 3;
+                        });
+                      },
+                      icon: Icon(
+                        Icons.person,
+                        color: grayColor,
+                      ))
+            ],
+          ),
         ),
       ),
+
+      // bottomNavigationBar: SizedBox(
+      //   height: 110,
+      //   child: ClipRRect(
+      //     borderRadius: BorderRadius.only(
+      //         topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+      //     child: BottomNavigationBar(
+      //         selectedItemColor: orangeColor,
+      //         onTap: (int index) {
+      //           setState(() {
+      //             _currentIndex = index;
+      //           });
+      //         },
+      //         currentIndex: _currentIndex,
+      //         showSelectedLabels: true,
+      //         showUnselectedLabels: false,
+      //         unselectedIconTheme: IconThemeData(color: grayColor),
+      //         items: [
+      //           BottomNavigationBarItem(
+      //               backgroundColor: Colors.white,
+      //               icon: Icon(
+      //                 Icons.home,
+      //               ),
+      //               activeIcon: Container(
+      //                 padding:
+      //                     EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      //                 decoration: BoxDecoration(
+      //                     color: orangeColor.withOpacity(0.1),
+      //                     borderRadius: BorderRadius.all(Radius.circular(50))),
+      //                 child: Icon(
+      //                   Icons.home,
+      //                   color: orangeColor,
+      //                 ),
+      //               ),
+      //               label: "Home"),
+      //           BottomNavigationBarItem(
+      //               icon: Icon(Icons.shopping_bag),
+      //               activeIcon: Container(
+      //                 padding:
+      //                     EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      //                 decoration: BoxDecoration(
+      //                     color: orangeColor.withOpacity(0.1),
+      //                     borderRadius: BorderRadius.all(Radius.circular(50))),
+      //                 child: Icon(
+      //                   Icons.shopping_bag,
+      //                   color: orangeColor,
+      //                 ),
+      //               ),
+      //               label: "Bag"),
+      //           BottomNavigationBarItem(
+      //               icon: Icon(Icons.favorite),
+      //               activeIcon: Container(
+      //                 padding:
+      //                     EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      //                 decoration: BoxDecoration(
+      //                     color: orangeColor.withOpacity(0.1),
+      //                     borderRadius: BorderRadius.all(Radius.circular(50))),
+      //                 child: Icon(
+      //                   Icons.favorite,
+      //                   color: orangeColor,
+      //                 ),
+      //               ),
+      //               label: "Favorite"),
+      //           BottomNavigationBarItem(
+      //               icon: Icon(Icons.person),
+      //               activeIcon: Container(
+      //                 margin: EdgeInsets.only(right: 8),
+      //                 padding:
+      //                     EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      //                 decoration: BoxDecoration(
+      //                     color: orangeColor.withOpacity(0.1),
+      //                     borderRadius: BorderRadius.all(Radius.circular(50))),
+      //                 child: Icon(
+      //                   Icons.person,
+      //                   color: orangeColor,
+      //                 ),
+      //               ),
+      //               label: "User")
+      //         ]),
+      //   ),
+      // ),
     );
   }
 }
